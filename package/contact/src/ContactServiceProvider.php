6dfa685d82b5fae214042867f3d37b0c4f83ed9e
Jordan831 <vicky.electron831@gmail.com>
2020-04-10 19:40:34 -0700

    This service Provider File

---
+++ package/contact/src/ContactServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+use Illuminate\Support\ServiceProvider;
+class ContactServiceProvider extends ServiceProvider
+{
+ public function boot(){
+
+ }
+
+ public function register()
+ {
+
+   
+ }
+
+}
+
+
+
+ ?>
